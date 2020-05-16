b587c142d4c16ecd28168b237f71564ef64ff2fe
Anton Ivlev <anton@codesigned.co.uk>
2020-05-16 13:40:31 +0100

    Video chat files

---
+++ main.js
@@ -0,0 +1,41 @@
+let state = {
+	stream: null,
+	id: null
+}
+
+let peer = new Peer(); 
+peer.on('open', (id) => {
+	state.id = id;
+	document.querySelector('#my-id').innerText = id;
+})
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+	.then(stream => {
+		addStreamToVid(stream, 'me');
+		state.stream = stream;
+	})
+	.catch(err => console.log(err));
+
+let addStreamToVid = (stream, videoID) => {
+	let vid = document.querySelector('#'+videoID);
+	vid.srcObject = stream;
+	vid.onloadedmetadata = () => vid.play();
+}
+
+let callFriend = () => {
+	let friendID = document.querySelector('#friend-id').value;
+	console.log('calling friend: ', friendID);
+
+	let call = peer.call(friendID, state.stream);
+	call.on('stream', (remoteStream) => {
+		console.log(remoteStream);
+		addStreamToVid(remoteStream, 'friend');
+	});
+}
+
+peer.on('call', (call) => {
+	call.answer(state.stream);
+	call.on('stream', (remoteStream) => {
+		addStreamToVid(remoteStream, 'friend');
+	});
+});
